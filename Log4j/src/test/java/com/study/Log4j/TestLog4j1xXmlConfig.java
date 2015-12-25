6d829ee02827ba4549df1a354b0ab8388b810155
Impler <704090860@qq.com>
2015-12-25 11:45:08 +0800

    remove useless code

--- Log4j/src/test/java/com/study/Log4j/TestLog4j1xXmlConfig.java
+++ Log4j/src/test/java/com/study/Log4j/TestLog4j1xXmlConfig.java
@@ -1,13 +1,12 @@
 package com.study.Log4j;
 
 import org.apache.log4j.Logger;
-import org.apache.log4j.xml.DOMConfigurator;
 import org.junit.Test;
 
 public class TestLog4j1xXmlConfig {
 	private static Logger log = Logger.getLogger(TestLog4j1xXmlConfig.class);
 	static{
-		DOMConfigurator.configure("src/main/resources/log4j.xml");
+		//DOMConfigurator.configure("src/main/resources/log4j.xml");
 	}
 	
 	
